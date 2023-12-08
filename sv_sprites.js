7effa924c16339f6f8ec8e785e40eed3ec8d0dde
zuki <55743069+zooki2006@users.noreply.github.com>
2023-12-08 02:15:52 +0000

    Create sv_sprites.js

---
+++ sv_sprites.js
@@ -0,0 +1,27 @@
+// @name         zuki's extra sv models for showdown script
+// @version      1
+// @description  a
+// @match        https://replay.pokemonshowdown.com/*
+// @match        https://play.pokemonshowdown.com/*
+// @grant        none
+// ==/UserScript==
+
+
+var interval = 0.3;
+//apend "https://play.pokemonshowdown.com/sprites/gen5/" to the name of the mon you want to exclude
+const excudedmons = ["https://play.pokemonshowdown.com/sprites/gen5/terapagos.png","https://play.pokemonshowdown.com/sprites/gen5/ironcrown.png","https://play.pokemonshowdown.com/sprites/gen5/ragingbolt.png","https://play.pokemonshowdown.com/sprites/gen5/archaludon.png","https://play.pokemonshowdown.com/sprites/gen5/MissingNo.png", "https://play.pokemonshowdown.com/sprites/gen5/sinistcha.png", "https://play.pokemonshowdown.com/sprites/gen5/sinistcha-masterpiece.png", "https://play.pokemonshowdown.com/sprites/gen5/ogerpon-tealtera.png", "https://play.pokemonshowdown.com/sprites/gen5/ogerpon.png", "https://play.pokemonshowdown.com/sprites/gen5/ogerpon-cornerstone.png", "https://play.pokemonshowdown.com/sprites/gen5/ogerpon-cornerstonetera.png", "https://play.pokemonshowdown.com/sprites/gen5/ogerpon-wellspring.png", "https://play.pokemonshowdown.com/sprites/gen5/ogerpon-wellspringtera.png", "https://play.pokemonshowdown.com/sprites/gen5/ogerpon-hearthflame.png", "https://play.pokemonshowdown.com/sprites/gen5/ogerpon-hearthflametera.png"];
+
+function imgReplace() {
+    var images = document.getElementsByTagName('img'),
+        len = images.length, img, i;
+    for (i = 0; i < len; i += 1) {
+        img = images[i];
+        if (img.src.includes('https://play.pokemonshowdown.com/sprites/gen5') && !img.src.includes('-back') && !img.src.includes('substitute') && !excudedmons.includes(img.src))
+        {
+            img.src = img.src.replace('https://play.pokemonshowdown.com/sprites/gen5/', 'https://raw.githubusercontent.com//zooki2006/SV_Sprites/main/padded/').replace('.png', '.gif');
+            img.src = img.src.replace('https://play.pokemonshowdown.com/sprites/gen5-shiny/', 'https://raw.githubusercontent.com//zooki2006/SV_Sprites/main/padded/').replace('.png', '-s.gif');
+      }
+    }
+}
+
+setInterval(imgReplace, interval * 1000);
